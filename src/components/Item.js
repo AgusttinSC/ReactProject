12d3d7f29691b045782238695e881c211c11ab71
Agustin <agusschiepelohut@gmail.com>
2022-11-15 00:18:15 -0300

    se agrega imagenes y se crea Detail

--- src/components/Item.js
+++ src/components/Item.js
@@ -7,6 +7,8 @@ const Item = ({productos}) => {
     <div>
         <p>{productos.title}</p>
         <p>{productos.description}</p>
+        <p>{productos.price}</p>
+        <img src={productos.pictureUrl} alt="VARIEDADESD DE SUSHI"></img>
     </div> 
     ))}
 </div>
