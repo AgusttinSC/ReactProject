ac323060bedc853f005941db1bf66212e9deee8e
Agustin <agusschiepelohut@gmail.com>
2022-11-27 19:52:12 -0300

    ultima actualizacion

--- src/assets/services/products.jsx
+++ src/assets/services/products.jsx
@@ -37,3 +37,4 @@ export const getProducts = () => {
     })
     return obtieneProductos;
 }
+
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,24 +2,25 @@ import React from 'react'
 import CardWidget from './CardWidget'
 import logo from '../assets/img/logo.jpg'
 import styles from '../assets/css/styles.css'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   const greeting = 'Bienvenido a mi tienda!'
   return (
     <div>
       <nav>
-        <a href="#"><img src={logo} alt="Logo" className='img-prueba' /></a>
+        <Link to = {'/'}><img src={logo} alt="Logo" className='img-prueba' /></Link>
 
           <ul className='ul-list'>
 
             <li className='li-item'>
-              <a href="#">Menú</a>
+              <Link to={`/menu/:id`}>Menú</Link>
             </li>
             <li className='li-item'>
-              <a href="#">Productos</a>
+            <Link to={`/Productos/:id`}>Productos</Link>
             </li>
             <li className='li-item'>
-              <a href="#">Sucursales</a>
+            <Link to={`/Sucursales/:id`}>Sucursales</Link>
             </li>
 
           </ul>
